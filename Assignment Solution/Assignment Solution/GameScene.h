06128fc8da3f227fb4d6bdb41475abce2c3bad68
c-hurrell <charliehurrell2805@gmail.com>
2023-05-02 04:01:40 +0100

    Set up assets with exe file
    Added README to Debug with .exe to show requirments

--- Assignment Solution/Assignment Solution/GameScene.h
+++ Assignment Solution/Assignment Solution/GameScene.h
@@ -3,11 +3,21 @@
 
 // GameObjects in Scene
 #include "Wall.h"
+// GAME MANAGER? Handles spawning and counting of meeplings, score tracking, Timer etc.
+// MEEPLING
+// POWER - SMITE
+// POWER - SHOCKWAVE
+// POWER - CLUSTER
+// HOUSE OBJECT? -> not needed for gameplay
+// TIMER
+// SCORE
 
 class GameScene :
     public Scene
 {
 public:
     GameScene();
+
+    int numberOfMeeplings = 100;
 };
 
---
+++ Assignment Solution/x64/Debug/README.txt
@@ -0,0 +1,8 @@
+Requirments:
+ - > assets
+ - > SDL2.dll
+ - > SDL2_image.dll
+ - > SDL2_mixer.dll
+ - > SDL2_tff.dll
+
+It's not perfect by any means but tbh Im proud of this engine so far
